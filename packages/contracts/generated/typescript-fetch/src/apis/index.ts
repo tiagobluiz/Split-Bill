7cb20285e30028a2632f420ae25f0e84436b382f
Tiago Luiz <tiagobluiz.97@gmail.com>
2026-03-01 14:23:40 +0000

    SB-033: add deterministic TypeScript client generation workflow
    - add contracts workspace package and pinned generator script
    
    - generate and commit typescript-fetch client artifacts
    
    - document regeneration process and change-control updates
    
    Fixes #39

---
+++ packages/contracts/generated/typescript-fetch/src/apis/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AnalyticsApi';
+export * from './AuthApi';
+export * from './BalancesApi';
+export * from './EntriesApi';
+export * from './EventsApi';
+export * from './InvitesApi';
+export * from './PeopleApi';
+export * from './ProfileApi';
+export * from './UploadsApi';
